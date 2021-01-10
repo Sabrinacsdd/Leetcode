a1a4aabd6b69caa01ad8f74c9723a771e34324cf
Sabrina <sabrinacs88@hotmail.com>
2021-01-10 11:24:08 -0800

    01/10/21

---
+++ test.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class test{
+
+    public static void main(String args[]) {
+        System.out.println("hello world");
+
+        Solution ans = new Solution();
+
+        int res = ans.check(5);
+
+        System.out.println(res);
+
+    }
+}
+
+class Solution {
+    public int check(int a) {
+        return a;
+    }
+
+}
